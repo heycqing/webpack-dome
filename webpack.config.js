3928b1ee9f6165e82c695bd5b76ec0fbdeba1520
SiqingIII@163.com <cqing@cqing.local>
2018-02-09 01:43:51 +0800

    添加命令 watch，来监控打包文件的修改

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     plugins:[
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title:'OUtput Management'
+            title:'Output Management'
         })
     ],
     output : {
