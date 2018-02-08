1673a9b15b8a65b72ccc0d379b4b8d6762683e6a
SiqingIII@163.com <cqing@cqing.local>
2018-02-09 00:37:44 +0800

    加载css资源
    添加style-loader , css-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
-
+import './style.css'
 //把lodash引入在这个文件中； 
 
 function component(){
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello','webpack'],' ');
+    element.classList.add('hello')
 
     return element;
 }
