86db05b0a191f1aad8495311ccb55e33f6d58abd
SiqingIII@163.com <cqing@cqing.local>
2018-02-09 16:48:17 +0800

    Tree Shaking 1
    在编写不同的函数后，只引用其中一个函数，经过webpack打包后，在app.bundle.js 文件中，依然会出现未引用的函数，这个问题是需要解决的，也就是我们需要在打包后，去掉未引用的东西，也就是Tree Shaking,摇动树，让不需要的落叶落下来；

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-//把lodash引入在这个文件中； 
-import _ from 'lodash'
-// 引入print.js
- import  print from './print.js'
-// 引入css文件
-import './style.css'
+// 引入math.js文件中的cube函数，但是并未引入square函数，
+// 在这里，有一个问题，就是打包后,bundle.js里也会包含未引用的
+// square函数，
+import  {cube} from './math.js'
 
 function component(){
-    var element = document.createElement('div');
+    var element = document.createElement('pre');
 
-    var btn = document.createElement('button')
-
-    element.innerHTML = _.join(['Hello','webpack'],' ');
-
-    btn.innerHTML = 'click me in that!';
-    btn.onclick = print;
-
-    element.appendChild(btn)
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to '+ cube(5)
+    ].join('\n\n')
     
     return element;
 }
--- src/print.js
+++
@@ -1,6 +0,0 @@
-export default function print(){
-    console.log("管理输出，print it!")
-    console.error("尝试错误输出！");
-    console.log("new something coming!")
-    
-}
