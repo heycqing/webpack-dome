4977a3540b2da3cca383d8dfb85352fcad7fe4a4
SiqingIII@163.com <cqing@cqing.local>
2018-02-23 14:26:46 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 // 引入热加载模块
 const webpack = require('webpack')
 // 引入去除不需要的代码插件，做到精简输出
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    entry : {
-        app:'./src/index.js',
-    },
+   
     devtool:'inline-source-map',
     devServer:{
        contentBase: './dist',
